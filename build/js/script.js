90bb9b0e0cb256a119760c2f77c2694eaf9752a9
Lesya <lesyaslash@gmail.com>
2020-10-16 15:17:12 +0300

    завершает работу

--- build/js/script.js
+++ build/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  // модалка
+  // ----------------модалка-----------------
   var modalOpenButton = document.querySelector('.page-header__item--modal');
   var modalCallbackOverlay = document.querySelector('.modal__call');
   var modalSuccessOverlay = document.querySelector('.modal__success');
@@ -91,7 +91,7 @@
     scrollOn();
   });
 
-  // отправка формы и открытие окна успешной отправки
+  // -----отправка формы и открытие окна успешной отправки------
   var modalForm = document.querySelector('.modal__form');
   var URL = 'https://echo.htmlacademy.ru';
   var name = modalForm.querySelector('[name=name]');
@@ -134,55 +134,319 @@
     scrollOn();
   });
 
-  // табы
-  var tabButtons = document.querySelectorAll('.programm__tab-item');
+  // отправка формы из блока "хочу поехать"
+  var wishForm = document.querySelector('.wish-to-go__form');
+  var wishPhone = wishForm.querySelector('[name=tel]');
 
-  /* // активный элемент в мобильной версии всегда по центру
-   var scrollToActiveItem = function (item) {
-     var scrollParent = item.parentNode;
-
-     // Ширина родителя — «окна», в котором прокручивается меню
-     var scrollParentWidth = scrollParent.clientWidth;
-
-     // Ширина активного пункта меню
-     var activeElWidth = item.clientWidth;
-
-     // Смещение активного пункта меню по горизонтали от левого края родителя
-     var activeElLeftOffset = item.offsetLeft;
-
-     // Проверяем, не влезает ли активный элемент в «окно»
-     var isActiveElInvisible = activeElLeftOffset + activeElWidth > scrollParentWidth;
-
-     if (isActiveElInvisible) {
-       // Прокручиваем родителя так, чтобы активный элемент стал точно по центру:
-       // scrollLeft = activeElLeftOffset прокрутит активный элемент к левому краю «окна»;
-       // scrollLeft = activeElLeftOffset - (scrollParentWidth / 2) прокрутит активный элемент
-       // так, что он станет левым краем по центру «окна».
-       scrollParent.scrollLeft = activeElLeftOffset - (scrollParentWidth / 2) + (activeElWidth / 2);
-     } else {
-       scrollParent.scrollLeft = 0;
-     }
-   };*/
-
-  tabButtons.forEach(function (button) {
-    button.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      var link = button.querySelector('a');
-      var tab = document.querySelector(link.getAttribute('href'));
-      var activeTab = document.querySelector('.programm__tab-item--active');
-
-      activeTab.classList.remove('programm__tab-item--active');
-      document.querySelector('.programm__content-item--active')
-          .classList.remove('programm__content-item--active');
-
-      button.classList.add('programm__tab-item--active');
-      tab.classList.add('programm__content-item--active');
-
-      /* if (window.matchMedia('(max-width: 767px)').matches) {
-        scrollToActiveItem(button);
-      }
-    });*/
-    });
+  wishForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    upload(new FormData(wishForm), uploadSuccessHandler);
+    if (isStorageSupport) {
+      localStorage.setItem('wishPhone', wishPhone.value);
+    }
+    wishForm.reset();
   });
+
+  // отправка формы из блока подробностей
+  var contactsForm = document.querySelector('.contacts__form');
+  var contactsName = contactsForm.querySelector('[name=name]');
+  var contactsPhone = contactsForm.querySelector('[name=tel]');
+  contactsForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    upload(new FormData(contactsForm), uploadSuccessHandler);
+    if (isStorageSupport) {
+      localStorage.setItem('contactsPhone', contactsPhone.value);
+      localStorage.setItem('ncontactsName', contactsName.value);
+    }
+    contactsForm.reset();
+  });
+
+  // ---------------табы------------------
+  var tabButtons = document.querySelectorAll('.program__tab-item');
+  var tabsHandler = function (evt) {
+    evt.preventDefault();
+    var link = evt.currentTarget.querySelector('a');
+    var tab = document.querySelector(link.getAttribute('href'));
+    var activeTab = document.querySelector('.program__tab-item--active');
+
+    activeTab.classList.remove('program__tab-item--active');
+    document.querySelector('.program__content-item--active')
+        .classList.remove('program__content-item--active');
+
+    evt.currentTarget.classList.add('program__tab-item--active');
+    tab.classList.add('program__content-item--active');
+  };
+
+  for (var i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].addEventListener('click', tabsHandler);
+  }
+
+  // ---------------слайдер--------------------
+  var slideShow = (function () {
+    return function (selector, config) {
+      var slider = document.querySelector(selector);
+      var sliderContainer = slider.querySelector('.slider__items');
+      var sliderItems = slider.querySelectorAll('.slider__item');
+      var sliderControls = slider.querySelectorAll('.slider__control');
+      var currentPosition = 0; // позиция левого активного элемента
+      var transformValue = 0; // значение транфсофрмации .slider_wrapper
+      var transformStep = 100; // величина шага (для трансформации)
+      var itemsArray = []; // массив элементов
+      var timerId;
+      var indicatorItems;
+      var indicatorIndex = 0;
+      var indicatorIndexMax = sliderItems.length - 1;
+      var stepTouch = 50;
+      var _config = {
+        isAutoplay: false, // автоматическая смена слайдов
+        directionAutoplay: 'next', // направление смены слайдов
+        delayAutoplay: 5000, // интервал между автоматической сменой слайдов
+        isPauseOnHover: true // устанавливать ли паузу при поднесении курсора к слайдеру
+      };
+
+      // настройка конфигурации слайдера в зависимости от полученных ключей
+      for (var key in config) {
+        if (key in _config) {
+          _config[key] = config[key];
+        }
+      }
+
+      // наполнение массива _itemsArray
+      for (var i = 0; i < sliderItems.length; i++) {
+        itemsArray.push({item: sliderItems[i], position: i, transform: 0});
+      }
+
+      // переменная position содержит методы, с помощью которой можно получить минимальный и максимальный индекс элемента, а также соответствующему этому индексу позицию
+      var position = {
+        getItemIndex: function (mode) {
+          var index = 0;
+          for (var j = 0; j < itemsArray.length; j++) {
+            if ((itemsArray[j].position < itemsArray[index].position && mode === 'min') || (itemsArray[j].position > itemsArray[index].position && mode === 'max')) {
+              index = j;
+            }
+          }
+          return index;
+        },
+        getItemPosition: function (mode) {
+          return itemsArray[position.getItemIndex(mode)].position;
+        }
+      };
+
+      // функция, выполняющая смену слайда в указанном направлении
+      var move = function (direction) {
+        var nextItem;
+        var currentIndicator = indicatorIndex;
+        if (direction === 'next') {
+          currentPosition++;
+          if (currentPosition > position.getItemPosition('max')) {
+            nextItem = position.getItemIndex('min');
+            itemsArray[nextItem].position = position.getItemPosition('max') + 1;
+            itemsArray[nextItem].transform += itemsArray.length * 100;
+            itemsArray[nextItem].item.style.transform = 'translateX(' + itemsArray[nextItem].transform + '%)';
+          }
+          transformValue -= transformStep;
+          indicatorIndex = indicatorIndex + 1;
+          if (indicatorIndex > indicatorIndexMax) {
+            indicatorIndex = 0;
+          }
+        } else {
+          currentPosition--;
+          if (currentPosition < position.getItemPosition('min')) {
+            nextItem = position.getItemIndex('max');
+            itemsArray[nextItem].position = position.getItemPosition('min') - 1;
+            itemsArray[nextItem].transform -= itemsArray.length * 100;
+            itemsArray[nextItem].item.style.transform = 'translateX(' + itemsArray[nextItem].transform + '%)';
+          }
+          transformValue += transformStep;
+          indicatorIndex = indicatorIndex - 1;
+          if (indicatorIndex < 0) {
+            indicatorIndex = indicatorIndexMax;
+          }
+        }
+        sliderContainer.style.transform = 'translateX(' + transformValue + '%)';
+        indicatorItems[currentIndicator].classList.remove('active');
+        indicatorItems[indicatorIndex].classList.add('active');
+      };
+
+      // функция, осуществляющая переход к слайду по его порядковому номеру
+      var moveTo = function (index) {
+        var k = 0;
+        var direction = (index > indicatorIndex) ? 'next' : 'prev';
+        while (index !== indicatorIndex && k <= indicatorIndexMax) {
+          move(direction);
+          k++;
+        }
+      };
+
+      // функция для запуска автоматической смены слайдов через промежутки времени
+      var startAutoplay = function () {
+        if (!_config.isAutoplay) {
+          return;
+        }
+        stopAutoplay();
+        timerId = setInterval(function () {
+          move(_config.directionAutoplay);
+        }, _config.delayAutoplay);
+      };
+
+      // функция, отключающая автоматическую смену слайдов
+      var stopAutoplay = function () {
+        clearInterval(timerId);
+      };
+
+      // функция, добавляющая индикаторы к слайдеру
+      var addIndicators = function () {
+        var indicatorsContainer = document.createElement('ol');
+        indicatorsContainer.classList.add('slider__indicators');
+        for (var l = 0; l < sliderItems.length; l++) {
+          var sliderIndicatorsItem = document.createElement('li');
+          if (l === 0) {
+            sliderIndicatorsItem.classList.add('active');
+          }
+          sliderIndicatorsItem.setAttribute('data-slide-to', l);
+          indicatorsContainer.appendChild(sliderIndicatorsItem);
+        }
+        slider.appendChild(indicatorsContainer);
+        indicatorItems = slider.querySelectorAll('.slider__indicators > li');
+      };
+
+      var isTouchDevice = function () {
+        return !!('ontouchstart' in window || navigator.maxTouchPoints);
+      };
+
+      // функция, осуществляющая установку обработчиков для событий
+      var setUpListeners = function () {
+        var startX = 0;
+        if (isTouchDevice()) {
+          slider.addEventListener('touchstart', function (evt) {
+            startX = evt.changedTouches[0].clientX;
+            startAutoplay();
+          });
+          slider.addEventListener('touchend', function (evt) {
+            var endX = evt.changedTouches[0].clientX;
+            var deltaX = endX - startX;
+            if (deltaX > stepTouch) {
+              move('prev');
+            } else if (deltaX < -stepTouch) {
+              move('next');
+            }
+            startAutoplay();
+          });
+        } else {
+          for (var t = 0; t < sliderControls.length; t++) {
+            sliderControls[t].classList.add('slider__control_show');
+          }
+        }
+        slider.addEventListener('click', function (evt) {
+          if (evt.target.classList.contains('slider__control')) {
+            evt.preventDefault();
+            move(evt.target.classList.contains('slider__control_next') ? 'next' : 'prev');
+            startAutoplay();
+          } else
+          if (evt.target.getAttribute('data-slide-to')) {
+            evt.preventDefault();
+            moveTo(parseInt(evt.target.getAttribute('data-slide-to'), 10));
+            startAutoplay();
+          }
+        });
+        document.addEventListener('visibilitychange', function () {
+          if (document.visibilityState === 'hidden') {
+            stopAutoplay();
+          } else {
+            startAutoplay();
+          }
+        }, false);
+        if (_config.isPauseOnHover && _config.isAutoplay) {
+          slider.addEventListener('mouseenter', function () {
+            stopAutoplay();
+          });
+          slider.addEventListener('mouseleave', function () {
+            startAutoplay();
+          });
+        }
+      };
+
+      // добавляем индикаторы к слайдеру
+      addIndicators();
+      // установливаем обработчики для событий
+      setUpListeners();
+      // запускаем автоматическую смену слайдов, если установлен соответствующий ключ
+      startAutoplay();
+
+      return {
+        // метод слайдера для перехода к следующему слайду
+        next: function () {
+          move('next');
+        },
+        // метод слайдера для перехода к предыдущему слайду
+        left: function () {
+          move('prev');
+        },
+        // метод отключающий автоматическую смену слайдов
+        stop: function () {
+          _config.isAutoplay = false;
+          stopAutoplay();
+        },
+        // метод запускающий автоматическую смену слайдов
+        cycle: function () {
+          config.isAutoplay = true;
+          startAutoplay();
+        }
+      };
+    };
+  }());
+
+  var removeIndicators = function (selector) {
+    var slider = document.querySelector(selector);
+    var indicatorsContainer = slider.querySelector('.slider__indicators');
+    if (indicatorsContainer) {
+      indicatorsContainer.parentNode.removeChild(indicatorsContainer);
+    }
+  };
+
+  var mediaQuery = window.matchMedia('(max-width: 1023px)');
+
+  // функция активирует слайдер или удаляет индикаторы при ресайзе окна
+  var sliderLife = function () {
+    if (mediaQuery.matches) {
+      removeIndicators('.life__slider');
+      slideShow('.life__slider', {
+        isAutoplay: false
+      });
+    } else {
+      removeIndicators('.life__slider');
+    }
+  };
+
+  sliderLife();
+  window.addEventListener('resize', sliderLife);
+
+  var sliderReviews = function () {
+    slideShow('.reviews__slider', {
+      isAutoplay: false
+    });
+    removeIndicators('.reviews__slider');
+  };
+
+  sliderReviews();
+
+  // -------------аккордеон---------------------
+
+  var accordion = document.querySelector('.faq__list');
+  var accordionQuestions = accordion.querySelectorAll('.faq__question');
+
+  var accordionCallback = function (evt) {
+    var parent = evt.target.parentElement;
+    parent.classList.toggle('faq__item--active');
+  };
+
+  if (accordion) {
+    accordion.classList.remove('faq__list--no-js');
+
+    for (var j = 0; j < accordionQuestions.length; j++) {
+      var item = accordionQuestions[j];
+      item.addEventListener('click', accordionCallback);
+    }
+  }
 
 })();
